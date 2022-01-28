28be981810d33b8e9937eb77309cee9187ebeb41
AnneRoki <59899443+AnneRoki@users.noreply.github.com>
2022-01-28 19:44:04 +0800

    yay

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-function getmeme(){
+function getpic(){
     murl ="https://waifu.pics/api/sfw/waifu"
     fetch(murl)
     .then(function(response){
@@ -6,14 +6,52 @@ function getmeme(){
         return response.json();
     })
     .then(function(data){
-        display_meme(data.url)
+        display_pic(data.url)
     })
     .catch(function(error){
         console.log("Error: " + error);
     });
 }
 
-function display_meme(image_url){
+function display_pic(image_url){
+    document.getElementById("waif").src =image_url
+
+}
+function getpic2(){
+    murl ="https://waifu.pics/api/sfw/hug"
+    fetch(murl)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function(data){
+        display_pic2(data.url)
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    });
+}
+
+function display_pic2(image_url){
+    document.getElementById("waif").src =image_url
+
+}
+function getpic3(){
+    murl ="https://waifu.pics/api/sfw/pat"
+    fetch(murl)
+    .then(function(response){
+        console.log(response)
+        return response.json();
+    })
+    .then(function(data){
+        display_pic3(data.url)
+    })
+    .catch(function(error){
+        console.log("Error: " + error);
+    });
+}
+
+function display_pic3(image_url){
     document.getElementById("waif").src =image_url
 
 }
